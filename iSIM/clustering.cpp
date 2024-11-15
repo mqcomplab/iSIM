8fa4554739051a3372d29b49f6b4111e9e334b47
Krisztina Zsigmond <144738961+kzsigmond@users.noreply.github.com>
2024-11-15 14:38:50 -0500

    Add tree class to clustering.

--- iSIM/clustering.cpp
+++ iSIM/clustering.cpp
@@ -1,44 +1,111 @@
 #include "clustering.h"
 
-
-struct processedFps{
-    Eigen::ArrayXXf data;
-    Eigen::ArrayXf n_objects;
-    std::list<std::list<int>> obj_indices;
+HCTreeNode::HCTreeNode(Eigen::ArrayXf fps, std::list<int> idx){
+    setFps(fps);
+    setIndices(idx);
+    setNObjects(idx.size());
+    leftPtr = NULL;
+    rightPtr = NULL;
 };
 
-struct processedFps pre_process(Eigen::ArrayXXf data){
-    processedFps processed_data;
-    processed_data.data = data;
-    processed_data.n_objects = Eigen::ArrayXf::Zero(data.rows())+1;
-    std::list<std::list<int>> obj_indices;
-    for (int i = 0; i < data.rows(); i++){
-        obj_indices.push_back({i});
-    }
-    return processed_data;
+Eigen::ArrayXf HCTreeNode::getFps(){
+    return fingerprints;
 }
 
-std::list<int> max_indices(struct processedFps fps, std::string n_ary){
-    int n_objects = fps.data.rows();
-    double max_sim = -3.08;
-    int max1 = n_objects;
-    int max2 = n_objects;
-    int cols = fps.data.cols();
-    #pragma omp parallel for
-    for(int i = 0; i < n_objects; i++){
-        for(int j = 0; j < n_objects; j++){
-            if(i != j){
-                Eigen::ArrayXf fp1 = fps.data.row(i);
-                Eigen::ArrayXf fp2 = fps.data.col(j);
-                int n = fps.n_objects[i] + fps.n_objects[j];
-                double sim = gen_sim_dict(fp1+fp2, n, 1.0)[n_ary];
-                if (sim > max_sim){
-                    max_sim = sim;
-                    max1 = i;
-                    max2 = j;
-                }
-            }
-        }
+void HCTreeNode::setFps(Eigen::ArrayXf fps){
+    fingerprints = fps;
+}
+
+HCTreeNode* HCTreeNode::getLeftPtr(){
+    return leftPtr;
+}
+
+void HCTreeNode::setLeftPtr(HCTreeNode* input_left){
+    leftPtr = input_left;
+}
+
+HCTreeNode* HCTreeNode::getRightPtr(){
+    return rightPtr;
+}
+
+void HCTreeNode::setRightPtr(HCTreeNode* input_right){
+    rightPtr = input_right;
+}
+
+void HCTreeNode::setNObjects(int n_mol){
+    n_objects = n_mol;
+}
+
+int HCTreeNode::getNObjects(){
+    return n_objects;
+}
+
+void HCTreeNode::setIndices(std::list<int> idx){
+    obj_indices=idx;
+}
+
+std::list<int> HCTreeNode::getIndices(){
+    return obj_indices;
+}
+
+
+
+HCTree::HCTree(){
+    root = NULL;
+}
+
+HCTreeNode* HCTree::getRoot(){
+    return root;
+}
+
+void HCTree::setRoot(HCTreeNode* input_root){
+    root = input_root;
+}
+
+void HCTree::insertRoot(Eigen::ArrayXf fps, std::list<int> idx){
+    root = new TreeNode(fps, idx);
+}
+
+Eigen::ArrayXf HCTree::getRootFingerprints(){
+    return root->getFps();
+}
+
+int HCTree::getRootNObjects(){
+    return root->getNObjects();
+}
+
+std::list<int> HCTree::getRootIndices(){
+    return root->getIndices();
+}
+
+void HCTree::combineTrees(HCTree tree_left, HCTree tree_right){
+    Eigen::ArrayXf fps = tree_left.getRootFingerprints() + tree_right.getRootFingerprints();
+    std::list<int> idx = tree_left.getRootIndices();
+    idx.splice(idx.end(), tree_right.getRootIndices());
+    idx.sort();
+    TreeNode* new_root = new TreeNode(fps, idx);
+    new_root->setLeft(tree_left.getRoot());
+    new_root->setRight(tree_right.getRoot());
+    
+    setRoot(new_root);
+}
+
+void HCTree::printTree(HCTreeNode* root, int space){
+    if (root==nullptr)
+        return;
+    space += COUNT;
+    printTree(root->rightPtr, space);
+
+    std::cout << std::endl;
+    for (int i = COUNT; i < space; i++)
+        std::cout << " ";
+    std::list<int> idx = root->getIndices();
+    std::cout << "(";
+    for (int i : idx){
+        std::cout << i << " ";
     }
-    return {max1, max2};
+    std::cout << ")\n";
+
+    printTree(root->leftPtr, space);
+
 }
--- iSIM/clustering.h
+++ iSIM/clustering.h
@@ -1,5 +1,68 @@
 #include "isim.h"
 #include "comp.h"
 
-struct processedFps pre_process(Eigen::ArrayXXf data);
-std::list<int> max_indices(struct processedFps fps, std::string n_ary);
+class HCTreeNode{
+    // Class for the hierarchical clustering tree node
+    // each node stores:
+    //     fingerprints: the columwise sum of the fingerprints
+    //     obj_indices: indices of the molecules in the cluster
+    //     n_objects: number of molecules in the cluster
+    // in addition to pointers to the left and right child nodes. 
+
+    friend class Tree;
+
+    private:
+        int n_objects;
+        Eigen::ArrayXf fingerprints;
+        std::list<int> obj_indices;
+        TreeNode* leftPtr;
+        TreeNode* rightPtr;
+        
+    public:
+        HCTreeNode(Eigen::ArrayXf fps, std::list<int> idx);
+
+        Eigen::ArrayXf getFps();
+
+        void setFps(Eigen::ArrayXf fps);
+
+        HCTreeNode* getLeftPtr();
+
+        void setLeftPtr(HCTreeNode* input_left);
+
+        HCTreeNode* getRightPtr();
+
+        void setRightPtr(HCTreeNode* input_right);
+
+        void setNObjects(int n_mol);
+
+        int getNObjects();
+
+        void setIndices(std::list<int> idx);
+
+        std::list<int> getIndices();
+};
+
+class HCTree{
+    private:
+        HCTreeNode* rootPtr;
+    public:
+        HCTree();
+
+        HCTreeNode* getRoot();
+
+        void setRoot(HCTreeNode* input_root);
+
+        void insertRoot(Eigen::ArrayXf fps, std::list<int> idx);
+
+        Eigen::ArrayXf getRootFps();
+
+        int getRootNObjects();
+
+        std::list<int> getRootIndices();
+
+
+        void combineTrees(HCTree tree_left, HCTree tree_right);
+
+        void printTree(HCTreeNode* root, int space);
+};
+
